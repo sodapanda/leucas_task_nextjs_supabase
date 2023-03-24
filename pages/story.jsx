60026b69d7ca3c1d711e5e84f9f84fc3d8d9e905
leucas <sodapanda20@gmail.com>
2023-03-24 23:46:37 +0800

    A

--- pages/story.jsx
+++ pages/story.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { useRouter } from 'next/router';
-import { Text, Divider, Group, Button } from '@mantine/core';
+import { Text, Divider, Group, Button, Box } from '@mantine/core';
+import { Skeleton } from '@mantine/core';
 
 export default function Story() {
   const user = useUser();
   const supabase = useSupabaseClient();
-  const router = useRouter();
   const [task, setTask] = useState({});
   const [idea, setIdea] = useState({});
   const [ideaMsg, setIdeaMsg] = useState('');
+  const [gptResult, setGptResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const apiKey = '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';
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -47,17 +49,88 @@ export default function Story() {
     setIdea(data[0]);
   }
 
+  async function callOpenAi(prompt, callback) {
+    setIsLoading(true);
+    const rst = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: `gpt-3.5-turbo`,
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+      }),
+    });
+
+    const response = await rst.json();
+    const result = response.choices[0].message.content;
+    callback(result);
+    setIsLoading(false);
+  }
+
   return (
-    <div className="w-full">
+    <Box component="div" className="w-full" px="xs">
       <Text>{ideaMsg}</Text>
       <Divider my="sm" variant="dashed" />
       <Text>{task.features}</Text>
       <Divider my="sm" variant="dashed" />
       <Group position="right">
-        <Button variant="outline">生成推广tweet</Button>
-        <Button variant="outline">生成分镜</Button>
-        <Button variant="outline">生成故事</Button>
+        <Button
+          variant="outline"
+          onClick={async () => {
+            await callOpenAi(
+              `产品名字叫做${task.task_name}\n 产品定位${ideaMsg} \n 产品功能 ${task.features} \n 请生成推广Tweet \n要求包含emoji,包含产品关键功能,不要太浮夸,用中文输出:`,
+              (result) => {
+                setGptResult(result);
+              }
+            );
+          }}
+        >
+          生成推广tweet
+        </Button>
+        <Button
+          variant="outline"
+          onClick={async () => {
+            await callOpenAi(
+              `产品名字叫做${task.task_name}\n 产品定位${ideaMsg} \n 产品功能 ${task.features} \n请生成推广视频分镜`,
+              (result) => {
+                setGptResult(result);
+              }
+            );
+          }}
+        >
+          生成分镜
+        </Button>
+        <Button
+          variant="outline"
+          onClick={async () => {
+            await callOpenAi(
+              `要求使用中文回答。产品名字叫做${task.task_name}\n 产品定位${ideaMsg} \n 产品功能 ${task.features} \n 请以推广${task.task_name}为目的生成一个故事。`,
+              (result) => {
+                setGptResult(result);
+              }
+            );
+          }}
+        >
+          生成故事
+        </Button>
       </Group>
-    </div>
+
+      {isLoading ? (
+        <Box mt="sm" component="div">
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={6} radius="xl" />
+          <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        </Box>
+      ) : (
+        <Text mt="sm">{gptResult}</Text>
+      )}
+    </Box>
   );
 }
